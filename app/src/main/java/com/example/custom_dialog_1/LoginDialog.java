dca2fb63aaac8af907569761d58f56e761927d10
Prasad <prasadrjadhav11@gmail.com>
2024-02-16 11:32:52 +0530

    16-2-2024

--- app/src/main/java/com/example/custom_dialog_1/LoginDialog.java
+++ app/src/main/java/com/example/custom_dialog_1/LoginDialog.java
@@ -9,54 +9,38 @@ import android.widget.Toast;
 
 public class LoginDialog extends Dialog {
 
-    private EditText edtUserame,edtPassword;
+    private EditText edtUsername, edtPassword;
     private Button btnLogin;
     private Context context;
-
-    interface OnLoginlistener{
-        void onSuccess(LoginDialog loginDialog);
-        void  onFail(LoginDialog loginDialog);
-    }
-
-    private OnLoginlistener onLoginlistener=null;
-
-    public  void setOnLoginlistener(OnLoginlistener onLoginlistener){
-        this.onLoginlistener=onLoginlistener;
-    }
-
-    public  LoginDialog(Context context){
+    public LoginDialog(Context context) {
         super(context);
-        this.context=context;
+        this.context = context;
         initViews();
+
         btnLogin.setOnClickListener(new BtnLoginClickListener());
     }
 
-    private  class BtnLoginClickListener implements View.OnClickListener{
+    private class BtnLoginClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            if (edtUserame.getText().toString().equals("bitcode")&&edtPassword.getText().toString().equals("bitcode")){
-                if (onLoginlistener !=null){
-                    onLoginlistener.onSuccess(LoginDialog.this);
-                }
-                else {
-                    if (onLoginlistener !=null){
-                        onLoginlistener.onFail(LoginDialog.this);
-                    }
-                }
+            if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode")) {
+                mt("Login success");
+                dismiss();
+            }
+            else {
+                mt("Login failed");
             }
         }
     }
 
-    private void mt(String text){
-        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+    private void mt(String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 
-    private void  initViews(){
+    private void initViews() {
         setContentView(R.layout.login_dialog);
-        edtUserame=findViewById(R.id.edtUsername);
-        edtPassword=findViewById(R.id.edtPassword);
-        btnLogin=findViewById(R.id.btnLogin);
-
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
     }
-
 }
